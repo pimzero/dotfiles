949c19013af452c8eec406016ee54aa8bf559143
Pierre Marsais <pmarsais@google.com>
2022-03-13 19:17:29 +0000

    shell: check if askpass exists before enforcing its use

--- ._shell/90-askpass.sh
+++ ._shell/90-askpass.sh
@@ -1,4 +1,6 @@
 if [ -n "$DISPLAY" ]; then
-	[ -z "$SUDO_ASKPASS" ] && export SUDO_ASKPASS="/usr/lib/ssh/ssh-askpass"
-	alias sudo='sudo -A'
+	if [ -x '/usr/lib/ssh/ssh-askpass' ]; then
+		[ -z "$SUDO_ASKPASS" ] && export SUDO_ASKPASS="/usr/lib/ssh/ssh-askpass"
+		alias sudo='sudo -A'
+	fi
 fi
