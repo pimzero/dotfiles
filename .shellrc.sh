9493ab637494417cd6fdcd4ef6b775f50d2f10f4
Pierre Marsais <pmarsais@google.com>
2022-03-25 02:04:57 +0000

    Move ._shell to .local/share/shell

--- .shellrc.sh
+++ .shellrc.sh
@@ -7,7 +7,7 @@ SHELL=$(readlink /proc/$$/exe 2>/dev/null || '')
 # ### Custom scripts:
 # All shells load "~/._shell/*.sh"
 # /bin/bash will also load "~/._shell/*.bash"
-for f in ~/._shell/*; do
+for f in ~/.local/share/shell/*; do
 	case "$f" in
 		*.bash) [ `basename $SHELL` = "bash" ] && . $f;;
 		*.sh) . $f;;
