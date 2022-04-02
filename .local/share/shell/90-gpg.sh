fff7a839ea9fd36d2d1febb372f7dc91cf0f469e
Pierre Marsais <pmarsais@google.com>
2022-04-02 18:53:31 +0100

    Do not run gpg-agent at each shell startup
    gpg-agent should be started automatically/socket activated by systemd.

--- .local/share/shell/90-gpg.sh
+++ .local/share/shell/90-gpg.sh
@@ -1,4 +1,4 @@
-eval $(gpg-agent --daemon --enable-ssh-support 2>/dev/null)
+#eval $(gpg-agent --daemon --enable-ssh-support 2>/dev/null)
 if [ -z "$SSH_AUTH_SOCK" ] ; then
 	SSH_AUTH_SOCK="/run/user/$(id -u)/gnupg/S.gpg-agent.ssh"; export SSH_AUTH_SOCK;
 fi
