60fbbf50b95ee88ac3906258f2d7519d2bd09475
Pierre Marsais <pmarsais@google.com>
2022-06-20 17:02:37 +0100

    Use gpgconf in order to get ssh socket path

--- .local/share/shell/90-gpg.sh
+++ .local/share/shell/90-gpg.sh
@@ -1,4 +1,4 @@
 #eval $(gpg-agent --daemon --enable-ssh-support 2>/dev/null)
 if [ -z "$SSH_AUTH_SOCK" ] ; then
-	SSH_AUTH_SOCK="/run/user/$(id -u)/gnupg/S.gpg-agent.ssh"; export SSH_AUTH_SOCK;
+	SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"; export SSH_AUTH_SOCK;
 fi
